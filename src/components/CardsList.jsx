917281c245dbb0d69ea79fff6d7b6c45d18facf4
Vihang Supare <vihang7660@gmail.com>
2022-12-20 22:58:49 +0530

    styled the cardsList component

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import Card from "./Card";
-import data from "../data";
+import "./cardsList.css";
+import { BsFilter } from "react-icons/bs";
 import { useCards } from "../CardsContext";
 
 export default function CardsList() {
   const state = useCards();
-  return state.cardsInfo.map((item) => (
+
+  let cardsList = state.cardsInfo.map((item) => (
     <Card
       name={item.name}
       budget_name={item.budget_name}
       spent={item.spent.value}
       available_to_spend={item.available_to_spend.value}
-      currency={'SGD'}
+      currency={"SGD"}
       owner_name={item.owner_name}
       card_type={item.card_type.toUpperCase()}
       expiry={item.expiry}
       key={item.id}
     />
   ));
+  return (
+    <main>
+      <button className="filter-button">
+        <BsFilter size={25} /> <div>Filter</div>
+      </button>
+      <div className="cardsList">{cardsList}</div>
+    </main>
+  );
 }
--- src/data.jsx
+++ src/data.jsx
@@ -3,7 +3,7 @@ export default [
   {
     name: "Mixmax",
     budget_name: "Software subscription",
-    owner_name: 'Vishal',
+    owner_name: "Vishal",
     id: nanoid(),
     owner_id: 1,
     spent: {
@@ -22,7 +22,7 @@ export default [
   {
     name: "Second",
     budget_name: "Hardware",
-    owner_name: 'Ron',
+    owner_name: "Ron",
     id: nanoid(),
     owner_id: 2,
     spent: {
@@ -41,7 +41,64 @@ export default [
   {
     name: "Third",
     budget_name: "Thingamajinga",
-    owner_name: 'James',
+    owner_name: "James",
+    id: nanoid(),
+    owner_id: 3,
+    spent: {
+      value: 148,
+      currency: "SGD",
+    },
+    available_to_spend: {
+      value: 30,
+      currency: "SGD",
+    },
+    card_type: "burner",
+    expiry: "9 feb",
+    limit: 100,
+    status: "active",
+  },
+  {
+    name: "Mixmax",
+    budget_name: "Software subscription",
+    owner_name: "Vishal",
+    id: nanoid(),
+    owner_id: 1,
+    spent: {
+      value: 100,
+      currency: "SGD",
+    },
+    available_to_spend: {
+      value: 1000,
+      currency: "SGD",
+    },
+    card_type: "burner",
+    expiry: "9 feb",
+    limit: 100,
+    status: "active",
+  },
+  {
+    name: "Second",
+    budget_name: "Hardware",
+    owner_name: "Ron",
+    id: nanoid(),
+    owner_id: 2,
+    spent: {
+      value: 100,
+      currency: "SGD",
+    },
+    available_to_spend: {
+      value: 1000,
+      currency: "SGD",
+    },
+    card_type: "burner",
+    expiry: "9 feb",
+    limit: 100,
+    status: "active",
+  },
+  {
+    name: "Third",
+    budget_name: "Thingamajinga",
+    owner_name: "James",
     id: nanoid(),
     owner_id: 3,
     spent: {
